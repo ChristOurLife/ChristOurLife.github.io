0ce8746a116c44fe71326cafaeed56a1fa03fa9b
ChristOurLife <catg184@duck.com>
2025-01-16 15:09:48 -0500

    Add labels.bat

---
+++ labels.bat
@@ -0,0 +1,11 @@
+@echo off  
+setlocal enabledelayedexpansion
+cd C:\ChristOurLife\Launcher
+goto %label%
+if errorLevel=1 (
+    echo Either this script is bugged (likely), or you launched labels.bat on its own. Please use the launcher.
+) 
+REM update these if you are forking your own repository to match list.txt's options
+------------------------------------------------------------------------------------
+:Toolbox
+curl -s -O https://christourlife.github.io/Christourlife-toolbox.bat
