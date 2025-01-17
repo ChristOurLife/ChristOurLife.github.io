c346f3cf8c0f74c00522f85c5db594ba87b6557c
ChristOurLife <catg184@duck.com>
2025-01-17 06:59:45 -0500

    Update labels.bat

--- labels.bat
+++ labels.bat
@@ -5,6 +5,7 @@ goto %label%
 if errorLevel=1 (
     echo Either this script is bugged (likely), or you launched labels.bat on its own. Please use the launcher.
 ) 
+timeout /t 3 >nul
 REM update these if you are forking your own repository to match list.txt's options
 ------------------------------------------------------------------------------------
 :Toolbox
