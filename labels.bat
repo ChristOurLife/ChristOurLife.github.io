be2a5b4e7cb8e2606d8f97309cbaeb0ed6d6f833
ChristOurLife <catg184@duck.com>
2025-01-17 20:26:13 -0500

    Update labels.bat

--- labels.bat
+++ labels.bat
@@ -15,5 +15,5 @@ REM update these if you are forking your own repository to match list.txt's opti
 ------------------------------------------------------------------------------------
 :Toolbox
 curl -s -O https://christourlife.github.io/Christourlife-toolbox.bat
-start "" cmd /c Christourlife-toolbox.bat
+start "Toolbox" cmd /c Christourlife-toolbox.bat
 exit
