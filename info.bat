959e15ffe97222370cc9ffa77613dc19806a586d
ChristOurLife <catg184@duck.com>
2025-01-26 13:52:02 -0500

    Delete info.bat

--- info.bat
+++
@@ -1,5 +0,0 @@
-
-echo This repository is for utilities that can be remotely managed by Github. This can be used for variable code changes, or other crap. I really don't know.
-timeout /t 3 >nul
-set /p Tricks="Want to see what I can do? (Y/N)"
-if /i "%Tricks%"=="Y" goto tricks else if /i "%Tricks%"=="N" curl christourlife.github.io/error
