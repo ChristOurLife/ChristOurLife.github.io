618c0f56c7613623d1cc0144fd5d8704291650b0
Gabriel <catg184@duck.com>
2025-06-05 10:07:13 -0400

    Modify download directory for OneDrive users

--- YT2MP3.bat
+++ YT2MP3.bat
@@ -1,13 +1,17 @@
 @echo off
 setlocal enabledelayedexpansion
-
+:1
 :: Prompt user for YouTube URL
 echo Enter YouTube URL:
 set /p yturl=
 
-:: Run yt-dlp with MP3 conversion
-yt-dlp -x --audio-format mp3 "!yturl!"
+:: Run yt-dlp and download MP3 track to Music folder
+yt-dlp -x --audio-format mp3 -o "%USERPROFILE%\Music\%%(title)s.%%(ext)s" "!yturl!"
 
 echo.
-echo Download complete!
+echo Download complete! Your music *should* be saved in your Music folder.
 pause
+set /p Would you like to download another track? (Y/N) =
+if /i "!Would you like to download another track? (Y/N)!"=="Y" goto 1
+if /i "!Would you like to download another track? (Y/N)!"=="N" exit
+exit
